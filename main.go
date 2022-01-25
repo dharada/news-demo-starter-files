5134efe36a6367077b1b2f91f37278d742e6b03a
dharada <dharada6@gmail.com>
2022-01-25 13:49:36 +0900

    update

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	requestBytes, error := httputil.DumpRequestOut(r, true)
+
+	//if error != nil {
+	//	w.Write([]byte(error.Error()))
+	//}
+
+	w.Write([]byte("<h1>Hello World!" + string(requestBytes) + r.UserAgent() + "</h1>"))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
 
